cc1a2b634c0139b8b746635eb4314cc8e3a66c0e
Admin <Admin@DESKTOP-B1EECD2.mshome.net>
2023-11-03 00:02:00 +0530

    Day Two Programs

---
+++ Day 2/BreakDemo.java
@@ -0,0 +1,14 @@
+
+public class BreakDemo {
+
+		public static void main(String[] args) {
+
+			for (int s = 5; s < 15; s++) {
+				if (s == 7)
+					break;
+				System.out.println(s);
+			}
+	 System.out.println("End of the program");
+		}
+	}
+
