df58f065e94b7531100b0baa34e74014c34e8bb8
Admin <Admin@DESKTOP-B1EECD2.mshome.net>
2023-11-02 23:58:08 +0530

    Day first program

---
+++ Day 1/DataTypeDemo.java
@@ -0,0 +1,31 @@
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+				int num=56;
+				System.out.println("The value of num is: "+num);
+				
+				int value1 = 64 / 2; 
+				System.out.println("value 1=" + value1);
+				
+				float value2 = 9676577876f / 2f;
+				System.out.println("value 2=" + value2);
+
+				
+				double value3 = 9676577876d / 2d;
+				System.out.println("value 3=" + value3);
+
+				
+				float value4 = 5/2.5f; 
+				System.out.println("value 4=" + value4);
+
+				
+				
+				int marker = 450;
+				double percentage = marker * 0.46f+56;
+				System.out.println("percentage :" + percentage);
+					
+			}	
+	}
+
+
---
+++ Day 1/IdentifiersDemo.java
@@ -0,0 +1,32 @@
+
+public class IdentifiersDemo {
+
+
+	public static void main(String[] args) {
+		
+		//Valid identifiers
+		
+		int no=12;
+		System.out.println(no);
+		
+		int no2=133;
+		System.out.println(no2);
+		
+		float no3=56.88f;
+		System.out.println(no3);
+		
+		double _no4=45367.99d;
+		System.out.println(_no4);
+		
+		int $no2=133;
+		System.out.println($no2);
+		
+		//invalid identifiers
+		//int no 7=45;
+		//int #no8=23;
+		//int for=89;
+
+	}
+
+}
+
