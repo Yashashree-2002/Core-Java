f2855cd6171b66292daeb45ba39fe389ebc7529d
Admin <Admin@DESKTOP-B1EECD2.mshome.net>
2023-11-03 00:04:53 +0530

    Day second program

---
+++ Day 2/CountineDemo.java
@@ -0,0 +1,13 @@
+
+public class CountineDemo {
+	public static void main(String[] args) {
+		for (int k = 1; k < 15; k++)
+		{
+		// Odd numbers are skipped
+		if (k%2 != 0)
+		continue;
+		// Even numbers are printed
+		System.out.print(k + " ");
+		}
+	}
+}
