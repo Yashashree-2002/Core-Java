7dab070e1483ac5f9b881872a5b36b10e2351f0e
Admin <Admin@DESKTOP-B1EECD2>
2023-11-03 09:43:45 +0530

    Day Two Programs

---
+++ Day 2/NestedIfElseDemo.java
@@ -0,0 +1,27 @@
+
+public class NestedIfElseDemo {
+
+		public static void main(String[] args) {
+			
+			int a=20,b=12,c=25;
+			if(a>b)  
+			{
+				if(a>c)//True
+				
+					System.out.println("a is the largest number"); //True
+				else
+					System.out.println("c is the largest number"); //False
+			}
+			else
+			{
+				if(b>c)
+					System.out.println("b is the largest number"); //True
+				else
+					System.out.println("c is the largest number"); //False
+			}
+
+		}
+
+	}
+
+
